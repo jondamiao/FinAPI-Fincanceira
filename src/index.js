ce956846d73f6f00e9699c2501b01485d822fc55
Jonatas Damião <jonatasdamiao@hotmail.com>
2022-08-28 22:53:57 -0300

    mais alguns ajustes.

--- src/index.js
+++ src/index.js
@@ -132,4 +132,12 @@ app.delete("/account", verifyifExistsAccountCPF, (request, response) =>{
     return response.status(200).json(customers);
 })
 
+app.get("/balance", verifyifExistsAccountCPF, (request, response) =>{
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement)
+
+    return response.json(balance);
+})
+
 app.listen(3333);
